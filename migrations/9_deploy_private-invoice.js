18d287f88b231e17f694f39e3ff7b2da31846dab
masaun <masanoriuno@gmail.com>
2020-06-23 17:32:43 +0900

    Create instance of ZkAsset and ZkAssetMintable in PrivateInvoice.sol

--- migrations/9_deploy_private-invoice.js
+++ migrations/9_deploy_private-invoice.js
@@ -1,5 +1,8 @@
 var PrivateInvoice = artifacts.require("PrivateInvoice");
 var IERC20 = artifacts.require("IERC20");
+const ZkAsset = artifacts.require('./ZkAsset.sol');
+const ZkAssetMintable = artifacts.require('./ZkAssetMintable.sol');
+
 
 //@dev - Import from exported file
 var contractAddressList = require('./contractAddress/contractAddress.js');
@@ -7,6 +10,8 @@ var tokenAddressList = require('./tokenAddress/tokenAddress.js');
 var walletAddressList = require('./walletAddress/walletAddress.js');
 
 const daiAddress = tokenAddressList["Kovan"]["DAI"];                /// DAI address on Rinkeby
+const _zkAsset = ZkAsset.address;
+const _zkAssetMintable = ZkAssetMintable.address;
 
 //const depositedAmount = web3.utils.toWei("0.15");    // 0.15 DAI which is deposited in deployed contract. 
 
@@ -14,7 +19,7 @@ module.exports = async function(deployer, network, accounts) {
 
     let ownerAddress = walletAddressList["WalletAddress1"];
 
-    await deployer.deploy(PrivateInvoice, daiAddress)
+    await deployer.deploy(PrivateInvoice, daiAddress, _zkAsset, _zkAssetMintable)
                   .then(async function(privateInvoice) {
                       if(ownerAddress && ownerAddress!="") {
                           console.log(`=== Transfering ownership to address ${ownerAddress} ===`)
